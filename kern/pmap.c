f64098fc7615e00f485c83910eab20f11b142c2a
Alyssa Marie Brewer <brewe110@eceprog5.ecn.purdue.edu>
2026-02-04 12:50:36 -0500

    page insert

--- kern/pmap.c
+++ kern/pmap.c
@@ -268,10 +268,36 @@ page_init(void)
 	// NB: DO NOT actually touch the physical memory corresponding to
 	// free pages!
 	size_t i;
+
+	// Calculate the ceiling once before the loop
+    physaddr_t firstFree = PADDR(boot_alloc(0));
+
 	for (i = 0; i < npages; i++) {
-		pages[i].pp_ref = 0;
-		pages[i].pp_link = page_free_list;
-		page_free_list = &pages[i];
+		// physical address of the current page 'i' to check ranges
+        physaddr_t pa = i * PGSIZE;
+
+		// Range 1: Physical page 0 is in use
+        if (i == 0) {
+            pages[i].pp_ref = 1;
+            pages[i].pp_link = NULL;
+        }
+
+		// Range 3 & 4: The IO hole [IOPHYSMEM, EXTPHYSMEM) 
+		// AND the used part of extended memory [EXTPHYSMEM, first_free_pa)
+		else if (pa >= IOPHYSMEM && pa < firstFree) {
+			pages[i].pp_ref = 1;
+			pages[i].pp_link = NULL;
+		}
+		// Range 2 & 4 (free parts): Everything else is free!
+		else {
+				pages[i].pp_ref = 0;
+				pages[i].pp_link = page_free_list;
+				page_free_list = &pages[i];
+		}
+
+		//pages[i].pp_ref = 0;
+		//pages[i].pp_link = page_free_list;
+		//page_free_list = &pages[i];
 	}
 }
 
@@ -291,7 +317,27 @@ struct PageInfo *
 page_alloc(int alloc_flags)
 {
 	// Fill this function in
-	return 0;
+
+	struct PageInfo *pp;
+
+    // 1. Check if we have any free pages left
+    if (page_free_list == NULL) {
+        return NULL;
+    }
+
+    // 2. "Pop" the page from the head of the list
+    pp = page_free_list;
+    page_free_list = pp->pp_link;
+
+    // 3. Clear the pp_link so page_free can catch double-frees
+    pp->pp_link = NULL;
+
+    // 4. Handle the ALLOC_ZERO flag
+    if (alloc_flags & ALLOC_ZERO) {
+        memset(page2kva(pp), 0, PGSIZE);
+    }
+
+    return pp;
 }
 
 //
@@ -304,6 +350,20 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
+
+	//Check if reference not zero
+	if (pp->pp_ref != 0) {
+        panic("page_free: attempt to free a page that still has %d references!", pp->pp_ref);
+    }
+
+	// Check if not null
+	if (pp->pp_link != NULL) {
+        panic("page_free: attempt to free a page that is already on the free list!");
+    }
+
+	// Add page to head 
+	pp->pp_link = page_free_list;
+    page_free_list = pp;
 }
 
 //
@@ -342,6 +402,7 @@ page_decref(struct PageInfo* pp)
 pte_t *
 pgdir_walk(pde_t *pgdir, const void *va, int create)
 {
+	
 	// Fill this function in
 	return NULL;
 }
@@ -392,7 +453,24 @@ int
 page_insert(pde_t *pgdir, struct PageInfo *pp, void *va, int perm)
 {
 	// Fill this function in
-	return 0;
+	pte_t *pte = pgdir_walk(pgdir, va, 1); //returns a pointer to PTE for linear address 'va'
+    if (pte == NULL) { // if page table couldn't be allocated
+        return -E_NO_MEM;
+    }
+
+	//pp->pp_ref should be incremented 
+	// increment the reference count of the new page before you remove the old one for corner case?
+	pp->pp_ref++;
+
+	// If there is already a page mapped at 'va', it should be page_remove()d.
+	if (*pte & PTE_P) {
+        page_remove(pgdir, va);
+    }
+
+	//Update
+	*pte = page2pa(pp) | perm | PTE_P;
+
+	return 0; //On success
 }
 
 //
