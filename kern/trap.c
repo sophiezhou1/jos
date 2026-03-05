ee5bbf7e2811ddd3e1cfeb991e85f6fa4a95e8d1
Alyssa Marie Brewer <brewe110@eceprog3.ecn.purdue.edu>
2026-03-05 15:27:39 -0500

    finished 5 and 6 (4 missing text)

--- kern/trap.c
+++ kern/trap.c
@@ -193,7 +193,15 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-
+	switch (tf->tf_trapno) {
+        case T_PGFLT:
+            page_fault_handler(tf);
+            return;
+            
+        case T_BRKPT:
+            monitor(tf);
+            return;
+    }
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
