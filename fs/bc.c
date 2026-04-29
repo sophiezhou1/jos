57ef37564e76ee873671981c2b267c592aed229a
Alyssa Marie Brewer <brewe110@eceprog1.ecn.purdue.edu>
2026-04-29 16:47:45 -0400

    E3 and 4 are done

--- fs/bc.c
+++ fs/bc.c
@@ -65,14 +65,9 @@ bc_pgfault(struct UTrapframe *utf)
         panic("in bc_pgfault, ide_read: %e", r);
     }
 
-	// Clear the dirty bit for the disk block page since we just read the
-	// block from disk
 	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) < 0)
 		panic("in bc_pgfault, sys_page_map: %e", r);
 
-	// Check that the block we read was allocated. (exercise for
-	// the reader: why do we do this *after* reading the block
-	// in?)
 	if (bitmap && block_is_free(blockno))
 		panic("reading free block %08x\n", blockno);
 }
--- fs/fs.c
+++ fs/fs.c
@@ -62,7 +62,26 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	//panic("alloc_block not implemented");
+	// Block 0 is the boot sector, Block 1 is the super block, 
+    // and Block 2 is the start of the bitmap.
+    uint32_t blockno;
+
+    // Scan disk for a free block
+    for (blockno = 0; blockno < super->s_nblocks; blockno++) {
+        if (block_is_free(blockno)) {
+            // Mark block as used. 
+            // We find the right 32-bit chunk of the bitmap, and clear specific bit to 0.
+            bitmap[blockno / 32] &= ~(1 << (blockno % 32));
+
+            flush_block(&bitmap[blockno / 32]);
+
+            // Return newly allocated block number
+            return blockno;
+        }
+    }
+
+    // If we get here, the disk is completely full
 	return -E_NO_DISK;
 }
 
